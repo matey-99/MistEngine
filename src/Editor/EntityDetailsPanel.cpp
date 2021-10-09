8d304b3bae85eac5cf2a4c68c78417cb8b12dc48
Mateusz Michalak <mat.michalak1999@gmail.com>
2021-10-09 10:13:40 +0200

    Replace ImGui::SliderFloat3 with ImGui::DragFloat3 in Details Panel

--- src/Editor/EntityDetailsPanel.cpp
+++ src/Editor/EntityDetailsPanel.cpp
@@ -19,17 +19,17 @@ void EntityDetailsPanel::Render()
     arr[0] = &m_Entity->GetTransform()->Position.x;
     arr[1] = &m_Entity->GetTransform()->Position.y;
     arr[2] = &m_Entity->GetTransform()->Position.z;
-    ImGui::SliderFloat3("Position", *arr, -3.0f, 3.0f);
+    ImGui::DragFloat3("Position", *arr, 0.5f);
 
     arr[0] = &m_Entity->GetTransform()->Rotation.x;
     arr[1] = &m_Entity->GetTransform()->Rotation.y;
     arr[2] = &m_Entity->GetTransform()->Rotation.z;
-    ImGui::SliderFloat3("Rotation", *arr, -180.0f, 180.0f);
+    ImGui::DragFloat3("Rotation", *arr, 1.0f, -180.0f, 180.0f);
 
     arr[0] = &m_Entity->GetTransform()->Scale.x;
     arr[1] = &m_Entity->GetTransform()->Scale.y;
     arr[2] = &m_Entity->GetTransform()->Scale.z;
-    ImGui::SliderFloat3("Scale", *arr, 0.1f, 1.0f);
+    ImGui::DragFloat3("Scale", *arr, 0.1f);
     ImGui::Dummy(ImVec2(0.0f, 10.0f));
 
     if (ImGui::Button("Close"))
